8e404699b3f8b0d2dee79921fb840438d58623f3
Kvkens <kvkens@qq.com>
2016-09-09 16:21:11 +0800

    remove:去除原有version版本替换webpack内置&去除dev模式sourceMap.

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,56 @@ var webpack = require('webpack');
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var env = require('yargs').argv.mode;
+var fs = require('fs');
+
 
 var libraryName = 'sparrow';
+var data = fs.readFileSync('./package.json', 'utf8');
+var packageObj = JSON.parse(data);
+var headerStr = '';
+headerStr += packageObj.name + ' v' + packageObj.version + '\r\n';
+headerStr += packageObj.description + '\r\n';
+headerStr += 'author : ' + packageObj.author + '\r\n';
+headerStr += 'homepage : ' + packageObj.homepage + '\r\n';
+headerStr += 'bugs : ' + packageObj.bugs.url;
+var plugins = [new webpack.BannerPlugin(headerStr)],
+    outputFile;
 
-var plugins = [],
-	outputFile;
-
-if(env === 'build') {
-	plugins.push(new UglifyJsPlugin({
-		minimize: true
-	}));
-	outputFile = libraryName + '.min.js';
+if (env === 'build') {
+    plugins.push(new UglifyJsPlugin({
+        minimize: true
+    }));
+    outputFile = libraryName + '.min.js';
 } else {
-	outputFile = libraryName + '.js';
+    outputFile = libraryName + '.js';
 }
 
 var config = {
-	entry: __dirname + '/js/index.js',
-	devtool: 'source-map',
-	output: {
-		path: __dirname + '/dist',
-		filename: outputFile,
-		//library: 'u',
-		libraryTarget: 'var',
-		umdNamedDefine: true
-	},
-	module: {
-		loaders: [{
-			test: /(\.jsx|\.js)$/,
-			loader: 'babel',
-			exclude: /(bower_components)/
-		}, {
-			test: /(\.jsx|\.js)$/,
-			loader: "eslint-loader",
-			exclude: /node_modules/
-		}]
-	},
-	resolve: {
-		root: path.resolve('./js'),
-		extensions: ['', '.js']
-	},
-	plugins: plugins
+    entry: __dirname + '/js/index.js',
+    // devtool: 'source-map',
+    output: {
+        path: __dirname + '/dist',
+        filename: outputFile,
+        //library: 'u',
+        libraryTarget: 'var',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [{
+            test: /(\.jsx|\.js)$/,
+            loader: 'babel',
+            exclude: /(bower_components)/
+        }, {
+            test: /(\.jsx|\.js)$/,
+            loader: "eslint-loader",
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        root: path.resolve('./js'),
+        extensions: ['', '.js']
+    },
+    plugins: plugins
 };
 
 module.exports = config;
