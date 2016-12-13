8ee52e2e1d8f5c04bb57cdd568537db4e043efee
onvno <leewei2020@126.com>
2016-12-13 14:49:41 +0800

    add: i18n

--- js/util/i18n.js
+++ js/util/i18n.js
@@ -4,9 +4,61 @@
  * Date	  : 2016-07-29 10:16:54
  */
 //import {uuii18n} from '?';//缺失故修改为default值
+
+// 从datatable/src/compatiable/u/JsExtension.js抽取
+window.getCurrentJsPath = function() {
+	var doc = document,
+	a = {},
+	expose = +new Date(),
+	rExtractUri = /((?:http|https|file):\/\/.*?\/[^:]+)(?::\d+)?:\d+/,
+	isLtIE8 = ('' + doc.querySelector).indexOf('[native code]') === -1;
+	// FF,Chrome
+	if (doc.currentScript){
+		return doc.currentScript.src;
+	}
+
+	var stack;
+	try{
+		a.b();
+	}
+	catch(e){
+		stack = e.fileName || e.sourceURL || e.stack || e.stacktrace;
+	}
+	// IE10
+	if (stack){
+		var absPath = rExtractUri.exec(stack)[1];
+		if (absPath){
+			return absPath;
+		}
+	}
+
+	// IE5-9
+	for(var scripts = doc.scripts,
+		i = scripts.length - 1,
+		script; script = scripts[i--];){
+		if (script.className !== expose && script.readyState === 'interactive'){
+			script.className = expose;
+			// if less than ie 8, must get abs path by getAttribute(src, 4)
+			return isLtIE8 ? script.getAttribute('src', 4) : script.src;
+		}
+	}
+}
+
+if (window.i18n) {
+    var scriptPath = getCurrentJsPath(),
+        _temp = scriptPath.substr(0, scriptPath.lastIndexOf('/')),
+        __FOLDER__ = _temp.substr(0, _temp.lastIndexOf('/'))
+    i18n.init({
+        postAsync: false,
+        getAsync: false,
+        fallbackLng: false,
+        ns: {namespaces: ['uui-trans']},
+        resGetPath: __FOLDER__ + '/locales/__lng__/__ns__.json'
+    })
+}
+
 var trans = function (key, dftValue) {
-    //return  uuii18n ?  uuii18n.t('uui-trans:' + key) : dftValue;
-    return dftValue;
-};
+    return  i18n ?  i18n.t('uui-trans:' + key) : dftValue
+}
 
 export {trans};
